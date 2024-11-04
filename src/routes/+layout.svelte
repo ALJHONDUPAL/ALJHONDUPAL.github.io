02a8150ecd24c5c8ddad629ee19be163ef6a3d8f
ALJHONDUPAL <brainmissing3@gmail.com>
2024-11-04 20:42:38 +0800

    try2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
-{@render children()}
+
+<Navbar>
+	<NavBrand href="/">
+	  <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+	</NavBrand>
+	<NavHamburger  />
+	<NavUl >
+	  <NavLi href="/">Home</NavLi>
+	  <NavLi href="/about">About</NavLi>
+	  <NavLi href="/docs/components/navbar">Navbar</NavLi>
+	  <NavLi href="/pricing">Pricing</NavLi>
+	  <NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+  </Navbar>
